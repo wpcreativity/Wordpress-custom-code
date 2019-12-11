c349536d13111fc76c625560264b9a31c56111a4
wpcreativity <58761152+wpcreativity@users.noreply.github.com>
2019-12-11 10:44:20 +0530

    dfgdsf
    fdgdsf

---
+++ custom-post-type.php
@@ -0,0 +1,36 @@
+<?php 
+/*
+* Create custom post type
+* For example - we need to create a custom post type for slider.
+* Use this code in functions.php file
+*/
+function slider_custom_post_type() {
+ 
+$labels = array(
+
+	'name' => __( 'slider' ),
+        'singular_name' => __( 'slider' ),
+        'all_items' => __( 'All slider'),
+        'view_item' => __( 'View slider'), 
+        'add_new_item' => __( 'Add slider '),
+        'add_new' => __( 'Add slider'),
+        'edit_item' => __( 'Edit slider'),
+        'update_item' => __( 'Update slider')         
+
+);
+
+$arys = array(
+
+	'labels' => $labels,
+	'public' => true,
+        'has_archive' => true,
+        'menu_icon' => 'dashicons-book-alt',
+        'rewrite' => array('slug' => 'slider'),
+        'supports' => array( 'title', 'editor', 'excerpt','thumbnail')
+
+);
+register_post_type( 'slider', $arys);
+}
+
+add_action('init', 'slider_custom_post_type');
+?>
---
+++ custom-taxonomy.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Taxonomy: slider Category.
+ * For example - Here, We are creating a custom taxonomy for slider post type.
+ * Use this code in functions.php file
+ */
+function register_slider_category() {
+
+$labels = [
+        "name" => "slider Category",
+        "singular_name" =>"slider Categories",
+];
+
+$args = [
+        "label" => "slider Category",
+        "labels" => $labels,
+        "public" => true,
+        "publicly_queryable" => true,
+        "hierarchical" => true,
+        "show_ui" => true,
+        "show_in_menu" => true,
+        "show_in_nav_menus" => true,
+        "query_var" => true,
+        "rewrite" => [ 'slug' => 'slider_category', 'with_front' => true, ],
+        "show_admin_column" => true,
+        "show_in_rest" => true,
+        "rest_base" => "slider_category",
+        "rest_controller_class" => "WP_REST_Terms_Controller",
+        "show_in_quick_edit" => true,
+        ];
+register_taxonomy( "slider_category", [ "slider" ], $args );
+}
+add_action( 'init', 'register_slider_category' );
+
+?>
