40a781a980736dce4b41bcfd61ed583d3b26bf64
wpcreativity <58761152+wpcreativity@users.noreply.github.com>
2020-01-14 11:24:28 +0530

    Update wp_nav_menu.php

--- wp_nav_menu.php
+++ wp_nav_menu.php
@@ -13,4 +13,20 @@
         'walker' => new wp_bootstrap_navwalker()
   );
   wp_nav_menu($nav_menu);
+
+  //add class in a tag on wp_menu
+  add_filter( 'nav_menu_link_attributes', function($atts) {
+          $atts['class'] = "nav-link";
+          return $atts;
+  }, 100, 1 );
+
+  //add class in li tag on wp_menu
+  function add_menu_class_on_li($classes, $item, $args) {
+    if($args->theme_location == 'primary') {
+      $classes[] = 'nav-item';
+    }
+    return $classes;
+  }
+  add_filter('nav_menu_css_class', 'add_menu_class_on_li', 1, 3);
+
 ?> 
